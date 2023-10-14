c2ae06120c6c7032f73fa934e1db5a732ac88cc6
Noormohammed Shikalgar <noor.shikalgar03@gmail.com>
2023-10-15 01:37:47 +0530

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 function IssuePage() {
-  return <div>IssuePage</div>;
+  return (
+    <div>
+      IssuePage
+      <Button>New Issue</Button>
+    </div>
+  );
 }
 
 export default IssuePage;
